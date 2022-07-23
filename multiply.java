51f339b4d13321bd14436b4dae75eeb9f323e608
Vijay Sharma <Vjshrma0519@gmail.com>
2022-07-23 16:14:45 +0530

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    Run | Debug
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        system.out.println("the product is " + (a*b));
+    }
+}
